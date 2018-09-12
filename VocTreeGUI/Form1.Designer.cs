262d64b4c01100d358dcdcd7c7577f74d504c974
Thomas-David Griedel <s73811@informatik.htw-dresden.de>
2018-09-12 12:40:07 +0200

    Projektdateien hinzufügen.

---
+++ VocTreeGUI/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace VocTreeGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxMain = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(482, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dataset";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(462, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "C:\\Users\\David\\Desktop\\VocTreeDatasets\\Dataset8";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 74);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(79, 66);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start Server";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(97, 74);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(79, 66);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop Server";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(42, 518);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(196, 154);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(244, 518);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(196, 154);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(446, 518);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(196, 154);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(648, 518);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(196, 154);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(850, 518);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(196, 154);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 9;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBoxMain
+            // 
+            this.pictureBoxMain.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxMain.Image")));
+            this.pictureBoxMain.Location = new System.Drawing.Point(363, 217);
+            this.pictureBoxMain.Name = "pictureBoxMain";
+            this.pictureBoxMain.Size = new System.Drawing.Size(370, 273);
+            this.pictureBoxMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxMain.TabIndex = 10;
+            this.pictureBoxMain.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 157);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(464, 22);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "C:\\Users\\David\\Desktop\\VocTreeDatasets\\Dataset512\\input\\GOPR2584_189_f1320.jpg";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(482, 154);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 29);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "...";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Query";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 217);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 66);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Perform Query!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(119, 675);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(324, 675);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(529, 675);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(726, 675);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(927, 675);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 17);
+            this.label7.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1103, 717);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBoxMain);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBoxMain;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ VocTreeGUI/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace VocTreeGUI
+{
+    public partial class Form1 : Form
+    {
+        string path = "C:\\Users\\David\\Desktop\\VocTreeDatasets\\Dataset8", qpath = "C:\\Users\\David\\Desktop\\VocTreeDatasets\\Dataset512\\input\\GOPR2584_189_f1320.jpg";
+        Process qProcess,sProcess;
+        System.Threading.Thread queryThread, startThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(Form1_FormClosing);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog ofd = new FolderBrowserDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                path = ofd.SelectedPath;
+                textBox1.Text = path;
+                string[] filesInDir = Directory.GetFiles(path); // should get config file
+                string[] dirsInDir = Directory.GetDirectories(path); // should get the dirs 
+                List<string> dirList = new List<string>(); 
+                List<string> fileList = new List<string>();
+
+                foreach (string s in filesInDir)
+                {
+                    string[] stmp = s.Split('\\');
+                    fileList.Add(stmp[stmp.Length - 1]);
+                }
+                if (!fileList.Contains("config.txt"))
+                {
+                    MessageBox.Show("Config File Missing");
+                }
+
+                foreach (string s in dirsInDir)
+                {
+                    string[] stmp = s.Split('\\');
+                    dirList.Add(stmp[stmp.Length - 1]);
+                }
+                if (!(dirList.Contains("data") && dirList.Contains("input") && dirList.Contains("results") && dirList.Contains("vocabulary")))
+                {
+                    DialogResult dr = MessageBox.Show("Database have to be created. Build it now?",
+                      "No Database", MessageBoxButtons.YesNo);
+                    switch (dr)
+                    {
+                        case DialogResult.Yes:
+                            if (dirList.Contains("vocabulary") && dirList.Contains("input") && fileList.Contains("config.txt"))
+                            {
+                                System.Threading.Thread startThread = new System.Threading.Thread(new System.Threading.ThreadStart(createDatabase));
+                                startThread.Start();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Something is missing...");
+                            }
+                                break;
+                        case DialogResult.No:
+                            break;
+                    }
+                }
+
+            }
+
+        }
+        private static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        private void createDatabase()
+        {
+            //Starting voctree as new Process
+            Process myProcess = new Process();
+            try
+            {
+                myProcess.StartInfo.FileName = "engine.exe";
+                myProcess.StartInfo.Arguments = "-build " + path;
+                myProcess.StartInfo.UseShellExecute = false;
+                myProcess.StartInfo.RedirectStandardOutput = true;
+                myProcess.StartInfo.RedirectStandardError = true;
+                myProcess.ErrorDataReceived += new DataReceivedEventHandler(OutputHandler);
+                //start Process
+                myProcess.Start();
+                myProcess.BeginErrorReadLine();
+                string output = myProcess.StandardOutput.ReadToEnd();
+                myProcess.WaitForExit();
+                MessageBox.Show(getBetween(output, "storing info", "voctree delete"));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private void startServ()
+        {
+            if (sProcess != null)
+            {
+                sProcess.Close();
+                sProcess.Kill();
+            }
+            //Starting voctreeServer as new Process
+            sProcess = new Process();
+            try
+            {
+                sProcess.StartInfo.FileName = "engine.exe";
+                sProcess.StartInfo.Arguments = "-start " + path;
+                sProcess.StartInfo.UseShellExecute = false;
+                sProcess.StartInfo.CreateNoWindow = true;
+                sProcess.StartInfo.RedirectStandardOutput = true;
+                sProcess.StartInfo.RedirectStandardError = true;
+                sProcess.ErrorDataReceived += new DataReceivedEventHandler(OutputHandler);
+                //start Process
+                sProcess.Start();
+                MessageBox.Show("Server Started");
+                sProcess.BeginErrorReadLine();
+                string output = sProcess.StandardOutput.ReadToEnd();
+                sProcess.WaitForExit();
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private void stopServ()
+        {
+            if (sProcess != null)
+            {
+                startThread.Abort();
+                sProcess.Kill();
+            }
+                
+        }
+
+        private void queryImage()
+        {
+            if (qProcess != null)
+            {
+                qProcess.Close();
+                qProcess.Kill();
+            }
+            //Starting voctreeQuery as new Process
+            qProcess = new Process();
+            try
+            {
+                qProcess.StartInfo.FileName = "engine.exe";
+                qProcess.StartInfo.Arguments = "-query " + path + " " + qpath;
+                qProcess.StartInfo.UseShellExecute = false;
+                qProcess.StartInfo.CreateNoWindow = true;
+                qProcess.StartInfo.RedirectStandardOutput = true;
+                qProcess.StartInfo.RedirectStandardError = true;
+                qProcess.ErrorDataReceived += new DataReceivedEventHandler(OutputHandler);
+                //start Process
+                qProcess.Start();
+                qProcess.BeginErrorReadLine();
+                string output = qProcess.StandardOutput.ReadToEnd();
+                qProcess.WaitForExit();
+
+                //Format the Output to extract the Images
+                String[] matches = output.Split('\n').Skip(4).ToArray();
+                String[] scores = new string[5];
+                Label[] lableVek = new Label[5];
+                PictureBox[] PicVek = new PictureBox[5];
+                lableVek[0] = label3;
+                lableVek[1] = label4;
+                lableVek[2] = label5;
+                lableVek[3] = label6;
+                lableVek[4] = label7;
+                PicVek[0] = pictureBox1;
+                PicVek[1] = pictureBox2;
+                PicVek[2] = pictureBox3;
+                PicVek[3] = pictureBox4;
+                PicVek[4] = pictureBox5;
+
+                for (int i = 0; matches[i] != "\r" && matches[i] != ""; i++){
+                    string[] temp = matches[i].Split(',');
+                    scores[i] = temp[0];
+                    matches[i] = temp[2].Remove(0,1).Replace('/','\\');
+                    //Show first 5 Image Results
+                    PicVek[i].Image = new Bitmap(path + matches[i]);
+                    //And top weights
+                    lableVek[i].Text = scores[i];
+                }
+
+
+                
+
+                
+                
+                MessageBox.Show(path + matches[0]);
+
+                qProcess.Close();
+                qProcess.Kill();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Sollte mit Hreads geläst werden, da sonst neuer prozess hauptprogramm blockiert?
+            startThread = new System.Threading.Thread(new System.Threading.ThreadStart(startServ));
+            startThread.Start();
+        }
+
+        static void OutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            if (!string.IsNullOrWhiteSpace(outLine.Data)){
+                MessageBox.Show(outLine.Data);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            startThread = new System.Threading.Thread(new System.Threading.ThreadStart(stopServ));
+            startThread.Start();
+            MessageBox.Show("Server Stopped!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread queryThread = new System.Threading.Thread(new System.Threading.ThreadStart(queryImage));
+            queryThread.Start();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            path = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            qpath = textBox2.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                qpath = ofd.FileName;
+                textBox2.Text = qpath;
+                pictureBoxMain.Image = new Bitmap(qpath);
+            }
+        }
+
+        private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+            // EventHandler for the X closing of the Form, trying to get rid of the voctree background process
+            stopServ();
+            Environment.Exit(Environment.ExitCode);
+        }
+    }
+}
